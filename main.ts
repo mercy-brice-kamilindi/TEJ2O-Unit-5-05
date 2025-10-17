63e9bf1b51a8565b18773eefe138fbf78229d289
mercy-brice-kamilindi <mercy.brice.kamilindi@stu.ocsb.ca>
2025-10-17 13:13:22 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,9 +9,9 @@
 // variable
 let neopixelStrip: neopixel.Strip = null
 
-// cleanse
+// cleanup
 basic.clearScreen()
-neopixelStrip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
@@ -19,18 +19,20 @@ neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
-// Trafic light
-input.onButtonPressed(Button.A, function() {
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.show()
-    basic.pause(1000)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
-    neopixelStrip.show()
-    basic.pause(1000)
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+input.onButtonPressed(Button.A, function () {
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
     neopixelStrip.show()
     basic.pause(1000)
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    neopixelStrip.show()
+    basic.pause(1000)
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    basic.pause(1000)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
 })
